06a1d80496dfbf6599c4dac0383f75e2b0c3b7dd
Shubham Saini <node_saini@Shivaya.local>
2022-04-24 12:55:46 +0530

    added validation

--- index.js
+++ index.js
@@ -5,11 +5,15 @@ const axios = require('axios');
 const config = require('config');
 
 function timestampConverter(timestamp) {
-    var months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+    var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
     const date = new Date(Number(timestamp));
     const year = date.getFullYear();
     const month = date.getMonth() < 10 ? `0${date.getMonth()}` : date.getMonth();
-    return { formattedDate: `${year}-${month}-01T00:00:00.000`, month: months[date.getMonth()], year};
+    return {
+        formattedDate: `${year}-${month}-01T00:00:00.000`,
+        month: months[date.getMonth()],
+        year
+    };
 }
 
 function createResponse(data, ignore, month, year) {
@@ -47,34 +51,55 @@ function createResponse(data, ignore, month, year) {
         }
         total += Number(data[key]);
     }
-    return { ...output, total };
+    return {
+        ...output,
+        total
+    };
 }
 
 app.get('/api/visitors', (req, res) => {
-    let {
-        date,
-        ignore
-    } = req.query;
-    let {formattedDate, month, year} = timestampConverter(date);
-    axios.get(config.get('host'))
-        .then(function (response) {
-            let {
-                data
-            } = response;
-            const result = data.filter(obj => obj.month == formattedDate);
-            if (result) res.json(createResponse(result[0], ignore, month, year));
-            else {
-                res.status(204);
-                res.send();
-            }
-        })
-        .catch(function (error) {
-            console.log(error);
-            res.status(500);
-            res.send({
-                message: "Internal Server Error"
+    try {
+        let {
+            date,
+            ignore
+        } = req.query;
+        if (!date) {
+            res.status(404);
+            return res.json({
+                message: `Required field date is missing`
             })
+        }
+        let {
+            formattedDate,
+            month,
+            year
+        } = timestampConverter(date);
+        axios.get(config.get('host'))
+            .then(function (response) {
+                let {
+                    data
+                } = response;
+                const result = data.filter(obj => obj.month == formattedDate);
+                if (result) res.json(createResponse(result[0], ignore, month, year));
+                else {
+                    res.status(204);
+                    res.send();
+                }
+            })
+            .catch(function (error) {
+                console.log(error);
+                res.status(500);
+                res.send({
+                    message: "Internal Server Error"
+                })
+            })
+    } catch (error) {
+        console.log(error);
+        res.status(500);
+        res.send({
+            message: "Internal Server Error"
         })
+    }
 })
 
 app.listen(port, () => {
